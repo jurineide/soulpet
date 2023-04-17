880eb5b618547093ad9d806cff6c94ac72bc745f
jurineide <jurineidesouza@gmail.com>
2023-04-17 16:52:50 -0300

    atualização

--- index.js
+++ index.js
@@ -6,11 +6,23 @@ const { connection, authenticate } = require("./database/database");
 
 authenticate(connection);
 const Cliente=require("./database/cliente");
-const Endereco=require("./database/cliente");
+const Endereco=require("./database/endereco");
 
 
+app.post("/clientes",async(req,res)=>{
+    const{nome, email, telefone, endereco}=req.body;
 
+    try{
+        const novo = await Cliente.create(
+            {nome,email,telefone,endereco},
+            {include:[Endereco]});
 
+        res.status(201).json(novo);
+    } catch (err){        
+        res.status(500).json({message:"um erro aconteceu."});
+
+    } 
+});
 
 
 app.listen(3000, () => {
